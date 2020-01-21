3b693f10810e5eab1b08f5fbf6dff37df3106c04
Arsenko <minsenka@gmail.com>
2020-01-21 21:41:43 +0300

    initial commit

---
+++ app/src/main/java/com/example/endlessactivitychange/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.endlessactivitychange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.time.Instant;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+    public void init(){
+        Button buttonBack= findViewById(R.id.buttonBack);
+        Button buttonForvard= findViewById(R.id.buttonForvard);
+        TextView outView=findViewById(R.id.outView);
+
+        Random rand=new Random();
+        outView.setText("http://myfile.org/"+rand.nextInt(100));
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        buttonForvard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
